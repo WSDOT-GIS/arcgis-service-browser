dbf1fccb173d2e5a7c049eaea816804772083715
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2018-12-20 11:52:00 -0800

    Now built with webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  mode: "production",
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  }
+}
+
+module.exports = config;
